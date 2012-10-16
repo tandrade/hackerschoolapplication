2cf04d1a384740224141f006e88b7174b5e40b52
S <s>
2012-10-15 21:29:49 -0700

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -60,7 +60,7 @@ public class Breakout extends GraphicsProgram {
 /* Method: run() */
 /** Runs the Breakout program. */
 	public void run() {
-		buildBricks(); 
+		buildRows(); 
 	} 
 	
 	private void buildRows() { 
