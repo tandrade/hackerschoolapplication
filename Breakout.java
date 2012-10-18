f21e3f606c44b87f8acee1df4701332905694104
S <s>
2012-10-18 13:40:44 -0700

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -90,7 +90,7 @@ public class Breakout extends GraphicsProgram {
 	private static final double Y_VEL = 10; 
 	
 	private void moveBall() { 
-		while(ball.getY() < BOX_HEIGHT) { 
+		while(ball.getY() < HEIGHT - (2*BALL_RADIUS)) { 
 			dropBall(0);
 		}
 	}
