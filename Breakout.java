f947a470fb084b9f9f9f072b1545e2f040121215
S <s>
2012-10-20 20:06:04 -0700

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -71,7 +71,7 @@ public class Breakout extends GraphicsProgram {
 		buildBar((getWidth() - PADDLE_WIDTH)/2, BOX_HEIGHT);
 		buildBall(BALL_X_START, BALL_Y_START);
 		waitForClick();
-		while (true) { 
+		while (box.getY() == BOX_HEIGHT) { 
 			checkForPaddle(); 
 			checkForXCollision();
 			checkForYCollision();
