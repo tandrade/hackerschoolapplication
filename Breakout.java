531f8132299b607b8487506ff0c0573e9f44596e
S <s>
2012-10-20 20:09:54 -0700

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -125,7 +125,7 @@ public class Breakout extends GraphicsProgram {
 	
 
 	private void checkForPaddle() { 
-		if (ball.getY() > BOX_HEIGHT - BALL_RADIUS && ball.getX() > box.getX() && ball.getX() < (box.getX() + PADDLE_WIDTH)) { 
+		if (ball.getY() > BOX_HEIGHT - 2*BALL_RADIUS && ball.getX() > box.getX() && ball.getX() < (box.getX() + PADDLE_WIDTH)) { 
 			dy = -dy;
 			pause(60); 
 		}
