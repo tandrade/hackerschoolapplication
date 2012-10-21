2212195a1cad9df792bafe39d4c9309ec81372d4
S <s>
2012-10-21 15:00:31 -0700

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -224,7 +224,7 @@ public class Breakout extends GraphicsProgram {
 	}
 	
 	private void checkBottom() {
-		GObject hitbrick = getElementAt(ball.getX(), ball.getY()); 
+		GObject hitbrick = getElementAt(ball.getX(), ball.getY() + 1); 
 		if (hitbrick != null) { 
 			if (hitbrick != ball && hitbrick != box) { 
 				dy = -dy; 
