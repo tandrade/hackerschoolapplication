417cb96b19ed42d2fd7392f433773f752227d96b
S <s>
2012-10-22 20:36:33 -0700

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -330,6 +330,9 @@ public class Breakout extends GraphicsProgram {
 			if (collision == box) dy = -VEL; 
 			else{ 
 				remove(collision);
+				if (collision == null) { 
+					dy = -dy; 
+				}
 			}
 		} 
 	}
