c09bdd0ad1ee29ca49b680c85edaedca1d358e72
S <s>
2012-10-23 16:31:36 -0700

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -321,7 +321,7 @@ public class Breakout extends GraphicsProgram {
 	private void bricksLeft() { 
 		for (int k=0; k > WIDTH; k = k + BRICK_WIDTH + BRICK_SEP) { 
 			for (int m= BRICK_Y_OFFSET; m > (NBRICK_ROWS*5 + 1); m++) { 
-				GObject test = getElementAt(k, j);
+				GObject test = getElementAt(k, m);
 				
 			}
 		}
