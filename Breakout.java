b9a9e417d2d63046114839f6e466fad0b7081705
S <s>
2012-10-23 15:07:11 -0700

    {"type":"TI", "runs":"2"}

--- Breakout.java
+++ Breakout.java
@@ -293,8 +293,8 @@ public class Breakout extends GraphicsProgram {
 	
 
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	private double X_VEL = rgen.nextDouble(1.0, 3.0);; 
-	private double Y_VEL = 1; 
+	private double X_VEL = rgen.nextDouble(1.0, 3.0); 
+	private double Y_VEL = rgen.nextDouble(1.0, 3.0); 
 	/* x and y movement of the ball */ 
 	private double dx = X_VEL;
 	private double dy = Y_VEL;
