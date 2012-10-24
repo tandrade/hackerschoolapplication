ca3942728f68d50e4c788fda831b3efa86cb9660
S <s>
2012-10-23 17:18:55 -0700

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -241,7 +241,7 @@ public class Breakout extends GraphicsProgram {
 	
 	private void checkObjCollision() { 
 		GObject collision = getCollidingObject();
-		if (collision != null && collision != scorecard) { 
+		if (collision != null && collision != scorecard && collision != livescard) { 
 			if (collision == box) Y_VEL = -Y_VEL; 
 			else{ 
 				remove(collision);
