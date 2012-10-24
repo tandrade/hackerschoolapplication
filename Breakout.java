60cee893e640c7a007a43aaa2e8261ee04f8fdd2
S <s>
2012-10-23 17:19:23 -0700

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -331,6 +331,7 @@ public class Breakout extends GraphicsProgram {
 	private GRect box; 
 	private GOval ball;
 	private GLabel scorecard;
+	private GLabel livesdisp;
 	private GObject collision; 
 
 	/* used to keep track of the ball in play */ 
