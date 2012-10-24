4a22b233b3ce58b8bd5b50b1cf8239bd8544e06f
S <s>
2012-10-23 17:17:10 -0700

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -101,6 +101,7 @@ public class Breakout extends GraphicsProgram {
 		buildBar((getWidth() - PADDLE_WIDTH)/2, BOX_HEIGHT);
 		buildBall(BALL_X_START, BALL_Y_START);
 		showScore();
+		showLives();
 	}
 	
 	
